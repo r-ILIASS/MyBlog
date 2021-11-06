86977d2f75987e7603b5b684dbde1550baf74b6b
r-ILIASS <iliass.root@gmail.com>
2021-11-06 15:25:07 +0100

    Feat: Populated <PostCard /> with GraphCMS data

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'title1', excerpt: 'First post contenct' },
-  { title: 'title2', excerpt: 'Second post contenct' },
-  { title: 'title3', excerpt: 'Third post contenct' },
-];
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8 ">
       <Head>
         <title>My Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post, index) => <PostCard post={post.node} key={post.title} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -26,4 +22,11 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts }
+  }
 }
