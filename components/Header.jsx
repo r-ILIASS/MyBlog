79b8d164d62d5239a3f07ecb9bcbb195b206bd07
r-ILIASS <iliass.root@gmail.com>
2022-01-24 16:13:51 +0100

    Prepare app for deployment

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,38 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { getCategories } from '../services';
-
+import React from "react";
+import Link from "next/link";
 
 const Header = () => {
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    getCategories()
-      .then((newCategories) => setCategories(newCategories))
-  }, []);
-
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="bg-gray-900 w-full inline-block py-4 px-2">
         <div className="md:flaot-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              My Blog
+              My-Blog
             </span>
           </Link>
-          <div className="hidden md:float-left md:contents">
-            {categories.map((category) => (
-              <Link key={category.slug} href={`/category/${category.slug}`}>
-                <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                  {category.name}
-                </span>
-              </Link>
-            ))}
-          </div>
+          <div className="hidden md:float-left md:contents"></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
